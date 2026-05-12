d5aac9df34c7daf8337524187557eb7caf6c4128
Seken-930 <146303974+Seken-930@users.noreply.github.com>
2026-05-13 00:39:51 +0900

    main.tsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,25 +1,24 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import Layout from './components/Layout' // さっき作ったLayoutをインポート
-import UserListPage from './UserListPage'
-import UserDetailPage from './UserDetailPage'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import Layout from './components/Layout'
+import UserListPage from './pages/UserListPage'
+import UserDetailPage from './pages/UserDetailPage'
+
+createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <BrowserRouter>
-            {/* すべての画面をLayoutで包む */}
-            <Layout>
-                <Routes>
+            <Routes>
+                <Route element={<Layout />}>
                     <Route path="/" element={<UserListPage />} />
                     <Route path="/users" element={<UserListPage />} />
                     <Route path="/user/:id" element={<UserDetailPage />} />
-                    {/* プロフィールや投稿画面も今後ここに足していきます */}
-                    <Route path="/profile" element={<div>プロフィール画面（作成中）</div>} />
-                    <Route path="/post" element={<div>投稿画面（作成中）</div>} />
-                </Routes>
-            </Layout>
+                    <Route path="/profile" element={<div className="p-10">プロフィール画面（開発中）</div>} />
+                    <Route path="/post" element={<div className="p-10">投稿画面（開発中）</div>} />
+                </Route>
+            </Routes>
         </BrowserRouter>
     </React.StrictMode>,
 )
